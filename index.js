4b6ac8d8c507a250a10d0bbb120a3c86dc9dfbbc
Lianghwa Jou <lianghwajou@Lianghwas-MacBook-Pro.local>
2020-03-11 22:19:26 -0700

    Created NetInterface class.

--- index.js
+++ index.js
@@ -1,11 +1,109 @@
-import { NativeModules } from 'react-native';
+import {NativeModules} from 'react-native';
 
-const { RNNetinterfaces } = NativeModules;
+const {RNNetinterfaces} = NativeModules;
+
+// net/if.h
+// #define IFF_UP          0x1             /* interface is up */
+// #define IFF_BROADCAST   0x2             /* broadcast address valid */
+// #define IFF_DEBUG       0x4             /* turn on debugging */
+// #define IFF_LOOPBACK    0x8             /* is a loopback net */
+// #define IFF_POINTOPOINT 0x10            /* interface is point-to-point link */
+// #define IFF_NOTRAILERS  0x20            /* obsolete: avoid use of trailers */
+// #define IFF_RUNNING     0x40            /* resources allocated */
+// #define IFF_NOARP       0x80            /* no address resolution protocol */
+// #define IFF_PROMISC     0x100           /* receive all packets */
+// #define IFF_ALLMULTI    0x200           /* receive all multicast packets */
+// #define IFF_OACTIVE     0x400           /* transmission in progress */
+// #define IFF_SIMPLEX     0x800           /* can't hear own transmissions */
+// #define IFF_LINK0       0x1000          /* per link layer defined bit */
+// #define IFF_LINK1       0x2000          /* per link layer defined bit */
+// #define IFF_LINK2       0x4000          /* per link layer defined bit */
+// #define IFF_ALTPHYS     IFF_LINK2       /* use alternate physical connection */
+// #define IFF_MULTICAST   0x8000          /* supports multicast */
+
+const IP_FLAGS = {
+  UP: 0x1 /* interface is up */,
+  BROADCAST: 0x2 /* broadcast address valid */,
+  DEBUG: 0x4 /* turn on debugging */,
+  LOOPBACK: 0x8 /* is a loopback net */,
+  POINTOPOINT: 0x10 /* interface is point-to-point link */,
+  NOTRAILERS: 0x20 /* obsolete: avoid use of trailers */,
+  RUNNING: 0x40 /* resources allocated */,
+  NOARP: 0x80 /* no address resolution protocol */,
+  PROMISC: 0x100 /* receive all packets */,
+  ALLMULTI: 0x200 /* receive all multicast packets */,
+  OACTIVE: 0x400 /* transmission in progress */,
+  SIMPLEX: 0x800 /* can't hear own transmissions */,
+  LINK0: 0x1000 /* per link layer defined bit */,
+  LINK1: 0x2000 /* per link layer defined bit */,
+  LINK2: 0x4000 /* per link layer defined bit */,
+  ALTPHYS: 0x4000 /* use alternate physical connection */,
+  MULTICAST: 0x8000 /* supports multic */,
+};
+
+// const NetInterfaces = {
+//     async getInterfaces() {
+//         return await RNNetinterfaces.getInterfaces();
+//     }
+// }
 
 const NetInterfaces = {
-    async getInterfaces() {
-        return await RNNetinterfaces.getInterfaces();
+  async getInterfaces() {
+    return (interfaces => {
+      let iflist = {};
+      for (const ifname in interfaces) {
+        iflist[ifname] = new NetInterface(interfaces[ifname]);
+      }
+      return iflist;
+    })(await RNNetinterfaces.getInterfaces());
+  },
+};
+
+class NetInterface {
+  constructor(netif) {
+    this.netif = {name: netif.name, flags: netif.flags};
+    if ('ipv4' in netif) {
+      this.netif['ipv4'] = {addr: netif.ipv4.addr, netmask: netif.ipv4.netmask};
+      if ('dstaddr' in netif.ipv4) {
+        if (this.isPointToPoint()) {
+          this.netif.ipv4['dstaddr'] = netif.ipv4.dstaddr;
+        } else {
+          this.netif.ipv4['brdaddr'] = netif.ipv4.dstaddr;
+        }
+      }
     }
+    if ('ipv6' in netif) {
+      this.netif['ipv6'] = {addr: netif.ipv6.addr, netmask: netif.ipv6.netmask};
+      if ('dstaddr' in netif.ipv6) {
+        if (this.isPointToPoint()) {
+          this.netif.ipv6['dstaddr'] = netif.ipv6.dstaddr;
+        } else {
+          this.netif.ipv6['brdaddr'] = netif.ipv6.dstaddr;
+        }
+      }
+    }
+  }
+  isBroadcast() {
+    return this.netif.flags & IP_FLAGS.BROADCAST;
+  }
+  isUp() {
+    return this.netif.flags & IP_FLAGS.UP;
+  }
+  isPointToPoint() {
+    return this.netif.flags & IP_FLAGS.POINTOPOINT;
+  }
+  hasIPv4() {
+    return 'ipv4' in this.netif;
+  }
+  getIPv4() {
+    return this.netif.ipv4;
+  }
+  hasIPv6() {
+    return 'ipv6' in this.netif;
+  }
+  getIPv6() {
+    return this.netif.ipv6;
+  }
 }
 
-export { NetInterfaces };
+export {NetInterfaces};
--- ios/RNNetinterfaces.m
+++ ios/RNNetinterfaces.m
@@ -14,6 +14,11 @@
 //#define IOS_VPN       @"utun0"
 #define IP_ADDR_IPv4    @"ipv4"
 #define IP_ADDR_IPv6    @"ipv6"
+#define IP_FLAGS        @"flags"
+#define IF_NAME         @"name"
+#define IP_ADDR         @"addr"
+#define IP_NETMASK      @"netmask"
+#define IP_DSTADDR      @"dstaddr"
 
 @import SystemConfiguration.CaptiveNetwork;
 
@@ -30,7 +35,7 @@ RCT_EXPORT_METHOD(getInterfaces:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
 
-    NSMutableDictionary *addresses = [NSMutableDictionary dictionaryWithCapacity:8];
+    NSMutableDictionary *netif = [NSMutableDictionary dictionaryWithCapacity:8];
 
     @try {
         struct ifaddrs *interfaces;
@@ -38,40 +43,110 @@ RCT_EXPORT_METHOD(getInterfaces:(RCTPromiseResolveBlock)resolve
             // Loop through linked list of interfaces
             struct ifaddrs *interface;
             for(interface=interfaces; interface; interface=interface->ifa_next) {
-                if(!(interface->ifa_flags & IFF_UP) /* || (interface->ifa_flags & IFF_LOOPBACK) */ ) {
-                    continue; // deeply nested code harder to read
-                }
                 const struct sockaddr_in *addr = (const struct sockaddr_in*)interface->ifa_addr;
+                const struct sockaddr_in *netmask = (const struct sockaddr_in*)interface->ifa_netmask;
+                const struct sockaddr_in *dstaddr = (const struct sockaddr_in*)interface->ifa_dstaddr;
                 char addrBuf[ MAX(INET_ADDRSTRLEN, INET6_ADDRSTRLEN) ];
+                char netmaskBuf[ MAX(INET_ADDRSTRLEN, INET6_ADDRSTRLEN) ];
+                char dstaddrBuf[ MAX(INET_ADDRSTRLEN, INET6_ADDRSTRLEN) ];
                 if(addr && (addr->sin_family==AF_INET || addr->sin_family==AF_INET6)) {
+                    
                     NSString *name = [NSString stringWithUTF8String:interface->ifa_name];
+                    if (!netif[name]) {
+                        netif[name] = [NSMutableDictionary dictionaryWithCapacity:8];
+                    }
+                    // netif[name][IP_FLAGS] = interface->ifa_flags;
+                    netif[name][IP_FLAGS] = [NSNumber numberWithUnsignedInt:interface->ifa_flags];
+                    netif[name][IF_NAME] = name;
                     NSString *type;
                     if(addr->sin_family == AF_INET) {
                         if(inet_ntop(AF_INET, &addr->sin_addr, addrBuf, INET_ADDRSTRLEN)) {
                             type = IP_ADDR_IPv4;
+                            netif[name][IP_ADDR_IPv4] = [NSMutableDictionary dictionaryWithCapacity:8];
+                            netif[name][IP_ADDR_IPv4][IP_ADDR] = [NSString stringWithUTF8String:addrBuf];
+                            if(netmask && inet_ntop(AF_INET, &netmask->sin_addr, netmaskBuf, INET_ADDRSTRLEN)) {
+                                netif[name][IP_ADDR_IPv4][IP_NETMASK] = [NSString stringWithUTF8String:netmaskBuf];
+                            }
+                            if(dstaddr && inet_ntop(AF_INET, &dstaddr->sin_addr, dstaddrBuf, INET_ADDRSTRLEN)) {
+                                netif[name][IP_ADDR_IPv4][IP_DSTADDR] = [NSString stringWithUTF8String:dstaddrBuf];
+                            }
                         }
                     } else {
                         const struct sockaddr_in6 *addr6 = (const struct sockaddr_in6*)interface->ifa_addr;
+                        const struct sockaddr_in6 *netmask6 = (const struct sockaddr_in6*)interface->ifa_netmask;
+                        const struct sockaddr_in6 *dstaddr6 = (const struct sockaddr_in6*)interface->ifa_dstaddr;
                         if(inet_ntop(AF_INET6, &addr6->sin6_addr, addrBuf, INET6_ADDRSTRLEN)) {
                             type = IP_ADDR_IPv6;
+                            netif[name][IP_ADDR_IPv6] = [NSMutableDictionary dictionaryWithCapacity:8];
+                            netif[name][IP_ADDR_IPv6][IP_ADDR] = [NSString stringWithUTF8String:addrBuf];
+                            if(netmask && inet_ntop(AF_INET6, &netmask6->sin6_addr, netmaskBuf, INET6_ADDRSTRLEN)) {
+                                netif[name][IP_ADDR_IPv6][IP_NETMASK] = [NSString stringWithUTF8String:netmaskBuf];
+                            }
+                            if(dstaddr && inet_ntop(AF_INET6, &dstaddr6->sin6_addr, dstaddrBuf, INET6_ADDRSTRLEN)) {
+                                netif[name][IP_ADDR_IPv6][IP_DSTADDR] = [NSString stringWithUTF8String:dstaddrBuf];
+                            }
                         }
-                    }
-                    if(type) {
-                        NSString *key = [NSString stringWithFormat:@"%@/%@", name, type];
-                        addresses[key] = [NSString stringWithUTF8String:addrBuf];
                     }
                 }
             }
             // Free memory
             freeifaddrs(interfaces);
         }
-        resolve(addresses);
+        resolve(netif);
 
     }@catch (NSException *exception) {
         resolve(NULL);
     }
 
 }
+
+
+// RCT_EXPORT_METHOD(getInterfaces:(RCTPromiseResolveBlock)resolve
+//                   rejecter:(RCTPromiseRejectBlock)reject)
+// {
+
+//     NSMutableDictionary *addresses = [NSMutableDictionary dictionaryWithCapacity:8];
+
+//     @try {
+//         struct ifaddrs *interfaces;
+//         if(!getifaddrs(&interfaces)) {
+//             // Loop through linked list of interfaces
+//             struct ifaddrs *interface;
+//             for(interface=interfaces; interface; interface=interface->ifa_next) {
+//                 if(!(interface->ifa_flags & IFF_UP) /* || (interface->ifa_flags & IFF_LOOPBACK) */ ) {
+//                     continue; // deeply nested code harder to read
+//                 }
+//                 const struct sockaddr_in *addr = (const struct sockaddr_in*)interface->ifa_addr;
+//                 char addrBuf[ MAX(INET_ADDRSTRLEN, INET6_ADDRSTRLEN) ];
+//                 if(addr && (addr->sin_family==AF_INET || addr->sin_family==AF_INET6)) {
+//                     NSString *name = [NSString stringWithUTF8String:interface->ifa_name];
+//                     NSString *type;
+//                     if(addr->sin_family == AF_INET) {
+//                         if(inet_ntop(AF_INET, &addr->sin_addr, addrBuf, INET_ADDRSTRLEN)) {
+//                             type = IP_ADDR_IPv4;
+//                         }
+//                     } else {
+//                         const struct sockaddr_in6 *addr6 = (const struct sockaddr_in6*)interface->ifa_addr;
+//                         if(inet_ntop(AF_INET6, &addr6->sin6_addr, addrBuf, INET6_ADDRSTRLEN)) {
+//                             type = IP_ADDR_IPv6;
+//                         }
+//                     }
+//                     if(type) {
+//                         NSString *key = [NSString stringWithFormat:@"%@/%@", name, type];
+//                         addresses[key] = [NSString stringWithUTF8String:addrBuf];
+//                     }
+//                 }
+//             }
+//             // Free memory
+//             freeifaddrs(interfaces);
+//         }
+//         resolve(addresses);
+
+//     }@catch (NSException *exception) {
+//         resolve(NULL);
+//     }
+
+// }
 
 
 @end
